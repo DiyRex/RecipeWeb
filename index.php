db51ef35afeb6057144398164eeb802b928a715e
thishmikashihara <dtsperera@students.nsbm.ac.lk>
2024-03-26 23:47:07 +0530

    done

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <title>Recipe</title>
 </head>
 <body>
-    
+    <div class="">Hello</div>
+    <div class="">world</div>
+    tyyfjfyt
+    <h1>user</h1>
+    <p></p>
+    <h1>Login ok</h1>
 </body>
 </html>
